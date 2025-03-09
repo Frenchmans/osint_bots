0fc39e87532b155ce6fb756ee16ca6a9b9d26e86
Frenchmans <desmaraisavis@gmail.com>
2025-03-09 12:21:11 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+token = 'xx:xxxx' # Токен
+loginbot = 'xxxx_bot' # Логин бота
+
+QIWI_PUBLICKEY = 'xxxxxx' #p2p public key
+QIWI_TOKEN = 'xxxxxxxx' # токен киви
+QIWI_NUMBER = '77777777777' # Номер киви для оплаты(без +)
+
+admin_id = 1111111111 # Ид
+
+price = 190 # Цена
